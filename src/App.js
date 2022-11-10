8fb807fb49e20a18413006ec51a4bf39bff60b84
ErosWang01 <wxheros@outlook.com>
2022-11-10 16:49:24 +1000

    drag and drop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import DragNDrop from './components/DragNDrop'
+
+const data = [
+	{title: 'group 1', items: ['1', '2', '3']},
+	{title: 'group 2', items: ['4', '5']},
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			 <header className="App-header">
+				<DragNDrop data={data}/>
+			</header>
+		</div>
+  	);
 }
 
 export default App;
